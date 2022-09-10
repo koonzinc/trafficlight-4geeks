9595d324b260770ca39e44b28c56a579b820164b
koonzinc <koonz.eth@gmail.com>
2022-09-09 20:48:15 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from 'react';
+import './App.css';
 
 function App() {
+  const [light, setLight] = useState(null)
+
+  let redClass = '';
+  let yellowClass = '';
+  let greenClass = '';
+
+  if (light == 'red'){
+    redClass = 'selected';
+  } else if (light == 'yellow'){
+    yellowClass = 'selected';
+  } else if (light == 'green'){
+    greenClass = 'selected';
+  }
+  
+
   return (
-    <div>
-      h
+    <div className="App">
+      <div className='trafficTop'></div>
+      <div className='trafficBody'>
+        <div className={'redLight ' + redClass} onClick={() => setLight('red')}></div>
+        <div className={'yellowLight ' + yellowClass} onClick={() => setLight('yellow')}></div>
+        <div className={'greenLight ' + greenClass} onClick={() => setLight('green')}></div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
